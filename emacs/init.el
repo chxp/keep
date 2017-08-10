05e4f7f7bb14e56357e6b0f0d67c80b50883a005
chxp-d <chxp101@126.com>
2017-08-10 15:39:54 +0800

    refactor:(emacs/init.el) 直接加载 org 生成的 el 文件

--- emacs/init.el
+++ emacs/init.el
@@ -1,8 +1,9 @@
-(require 'package)
-(package-initialize)
+;; (require 'package)
+;; (package-initialize)
 
-(require 'org)
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "~/syncthing/emacs/_emacs.org"))
+;; (require 'org)
+;; (require 'ob-tangle)
+;; ;; 可以在 *scratch* Buffer 中运行此句，随后只需加载 _emacs.el
+;; (org-babel-load-file (expand-file-name "~/syncthing/emacs/_emacs.org"))
 
-;; (load-file "~/syncthing/emacs/_emacs.el")
+(load-file "~/syncthing/emacs/_emacs.el")
